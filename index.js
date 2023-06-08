54f457353667247d91fa7ffc7adb252007c2a8c7
md muhaiminul <muhai4959@gmail.com>
2023-06-08 21:28:09 +0600

    added user post method

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const app = express();
 const cors = require("cors");
-const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
+const app = express();
 const port = process.env.PORT || 5000;
+const jwt = require("jsonwebtoken");
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.njwxpap.mongodb.net/?retryWrites=true&w=majority";`;
+const verifyJWT = (req, res, next) => {
+	const authorization = req.headers.authorization;
+	if (!authorization) {
+		return res
+			.status(401)
+			.send({ error: true, message: "unauthorized access" });
+	}
+	// bearer token
+	const token = authorization.split(" ")[1];
+
+	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+		if (error) {
+			return res
+				.status(401)
+				.send({ error: true, message: "unauthorized access" });
+		}
+		req.decoded = decoded;
+		next();
+	});
+};
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.njwxpap.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -19,6 +40,9 @@ const client = new MongoClient(uri, {
 		strict: true,
 		deprecationErrors: true,
 	},
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	maxPoolSize: 60,
 });
 
 async function run() {
@@ -26,9 +50,35 @@ async function run() {
 		// Connect the client to the server	(optional starting in v4.7)
 		await client.connect();
 
+		//db pool
+		const usersCollection = client.db("yogaMeditation").collection("users");
 
-        const usersCollection = client.db("yogaMeditation").collection("users");
+		//jwt token
+		app.post("/jwt", (req, res) => {
+			const user = req.body;
+			const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+				expiresIn: "5h",
+			});
 
+			res.send({ token });
+		});
+
+		//add users info in db
+		app.post("/users", async (req, res) => {
+			console.log(req.body);
+			const user = req.body;
+			const query = { email: user.email };
+			const existingUser = await usersCollection.findOne(query);
+
+			if (existingUser) {
+				return res.send({ message: "user already exists" });
+			}
+
+			const result = await usersCollection.insertOne(user);
+			res.send(result);
+		});
+
+		
 
 		// Send a ping to confirm a successful connection
 		await client.db("admin").command({ ping: 1 });
