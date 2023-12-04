2f40a318ff91f1281dfa96562a88358e31861eba
Md. Muhaiminul <108287484+aargon007@users.noreply.github.com>
2023-12-04 17:52:36 +0600

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY);
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5050;
 const jwt = require("jsonwebtoken");
 
 // middleware
